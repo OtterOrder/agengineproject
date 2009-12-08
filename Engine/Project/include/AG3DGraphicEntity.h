65e3bce4e3deb3f402e904dd3eb725f8380728a0
alban.chagnoleau <alban.chagnoleau@fe70d4ac-e33c-11de-8d18-5b59c22968bc>
2009-12-08 21:48:23 +0000

    Ajout de la classe GraphicEntity

--- Engine/Project/include/AG3DGraphicEntity.h
+++ Engine/Project/include/AG3DGraphicEntity.h
@@ -1,9 +1,10 @@
 #pragma once
 
+#include "AGGraphicEntity.h"
 #include "AG3DEntity.h"
 
 //------------------------------------------------------------------------------------------------------------------------------
-class AG3DGraphicEntity : public AG3DEntity
+class AG3DGraphicEntity : public AG3DEntity, public AGGraphicEntity
 {
 public:
 	AG3DGraphicEntity	();
