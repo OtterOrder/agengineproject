15272fcb2f64339bd52f3da8cf8d184012ad5dcd
alban.chagnoleau <alban.chagnoleau@fe70d4ac-e33c-11de-8d18-5b59c22968bc>
2009-12-09 23:13:00 +0000

    Ajout de classes de ressources

---
+++ Engine/Project/include/AGSound.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "AGResource.h"
+
+//------------------------------------------------------------------------------------------------------------------------------
+class AGSound : public AGResource
+{
+public:
+	AGSound		();
+	~AGSound	();
+};
