400151a0cc61cb205fdfc61f1cc900186b1fb3bd
alban.chagnoleau <alban.chagnoleau@fe70d4ac-e33c-11de-8d18-5b59c22968bc>
2009-12-20 20:11:17 +0000

    Ajout du Device Manager

--- Engine/Project/include/AGMesh.h
+++ Engine/Project/include/AGMesh.h
@@ -1,9 +1,22 @@
 #pragma once
+
 #include "AGResource.h"
+#include "AGDeviceManager.h"
 
 //------------------------------------------------------------------------------------------------------------------------------
 class AGMesh : public AGResource
 {
+private:
+	u32						_mNbVertices;
+	u32						_mNbFaces;
+
+	AGPVertexBuffer			_mpVB;
+	AGPIndexBuffer			_mpIB;
+	AGVertexElement			_mVertexElements[AGMaxDecLength];	
+	AGPVertexDeclaration	_mpVD;
+
+	u32						_mVertexSize;
+
 public:
 	AGMesh		();
 	~AGMesh		();
