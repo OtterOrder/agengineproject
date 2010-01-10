cba01f2ccd9c9d86dd5af5de51c1df32980fa8c2
alban.chagnoleau <alban.chagnoleau@fe70d4ac-e33c-11de-8d18-5b59c22968bc>
2010-01-10 18:44:04 +0000

    Gestion de fenêtre

--- Engine/Modules/DeviceManager/src/AGWindowManager.cpp
+++ Engine/Modules/DeviceManager/src/AGWindowManager.cpp
@@ -1,13 +1,34 @@
 #include "AGWindowManager.h"
 
+
+//------------------------------------------------------------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
 //------------------------------------------------------------------------------------------------------------------------------
 void AGWindowManager::Initialize()
 {
-	WNDCLASSEX WindowClass = { sizeof(WNDCLASSEX), CS_CLASSDC, NULL/*MsgProc*/, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _mName, NULL };	// ToDo : Set MsgProc to InputManager
-	RegisterClassEx( &WindowClass );
+	WNDCLASSEX wcex;
 
-	_mWindow = CreateWindow( _mName, _mName, WS_OVERLAPPED, 0, 0, _mSize.x, _mSize.y, NULL, NULL, WindowClass.hInstance, NULL );
+	wcex.cbSize			= sizeof(WNDCLASSEX);
+	wcex.style			= CS_CLASSDC;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= GetModuleHandle(NULL);
+	wcex.hIcon			= NULL;
+	wcex.hCursor		= NULL;
+	wcex.hbrBackground	= NULL;
+	wcex.lpszMenuName	= NULL;
+	wcex.lpszClassName	= _mName;
+	wcex.hIconSm		= NULL;
 
-	ShowWindow( _mWindow, SW_NORMAL );
+	RegisterClassEx(&wcex);
+
+	_mWindow = CreateWindow( _mName, _mName, WS_OVERLAPPEDWINDOW, 0, 0, (s32)(_mSize.x), (s32)(_mSize.y), NULL, NULL, wcex.hInstance, NULL );
+
+	ShowWindow( _mWindow, SW_NORMAL);
 	UpdateWindow( _mWindow );
 }
