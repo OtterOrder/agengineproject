3789b44dbf02aef2b4bb1da71771a260bbd89924
alban.chagnoleau <alban.chagnoleau@fe70d4ac-e33c-11de-8d18-5b59c22968bc>
2010-01-10 19:48:32 +0000

    Chargement des fichiers .x

--- trunk/Engine/Project/include/AGResourceManager.h
+++ trunk/Engine/Project/include/AGResourceManager.h
@@ -45,7 +45,7 @@ public:
 
 			if( pResource->Load(_FileName, _Parameters) != AGResource::RES_SUCCEED )
 			{
-				AGWarning("Resource load failed");
+				AGWarning(false, "Resource load failed");
 				SAFE_DELETE(pResource);
 				return NULL;
 			}
