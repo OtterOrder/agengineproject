ef3665318683122e9cee5d6c09d931d9b788d282
alban.chagnoleau <alban.chagnoleau@fe70d4ac-e33c-11de-8d18-5b59c22968bc>
2010-01-10 02:01:52 +0000

    Ajout de fichiers de gestion de fenêtre

---
+++ Engine/Modules/DeviceManager/include/AGWindowManager.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "AGTypes.h"
+#include "AGSingleton.h"
+#ifdef AGDX9
+#include "AGDeviceManagerDX9.h"
+#endif
+
+#include <Windows.h>
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+class AGWindowManager : public AGSingleton<AGWindowManager>
+{
+friend class AGSingleton<AGWindowManager>;
+
+private:
+	AGVector2u	_mSize;
+	cStr		_mName;
+	HWND		_mWindow;
+
+
+protected:
+	AGWindowManager		()	{};
+	~AGWindowManager	()	{};
+
+public:
+	//
+	void	Initialize ();
+
+	// Resolution
+	inline	const u32&			GetWidth	()	const	{ return _mSize.x; };
+	inline	const u32&			GetHeight	()	const	{ return _mSize.y; };
+	inline	const AGVector2u&	GetSize		()	const	{ return _mSize; };
+
+	inline void		SetWidth	(u32 _x)			{ _mSize.x = _x; };
+	inline void		SetHeight	(u32 _y)			{ _mSize.x = _y; };
+	inline void		SetSize		(AGVector2u _Size)	{ _mSize = _Size; };
+	inline void		SetSize		(u32 _x, u32 _y)	{ _mSize.x = _x; _mSize.y = _y; };
+
+	// Name
+	inline const cStr&	GetName ()	const		{ return _mName; };
+	inline void			SetName	(cStr _Name)	{ _mName = _Name; };
+};
